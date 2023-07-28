a08b7c7ac5f870f5aa750a8c98da68abf21eab49
Arina Kunashko <kunashkoa@gmail.com>
2023-07-28 14:18:04 +0200

    add jest, supertest and github-actions

---
+++ api.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest')
+const agent = request('https://www.themealdb.com/api/json/v1/1/')
+
+
+describe("check the responses from api /search", function() {
+  it("Тест 1.1", function(done) {
+    agent
+     .get('/search.php')
+     .expect(200, done)
+  })
+})
+
+describe("check the responses from api /random", function() {
+    it("Тест 1.2", function(done) {
+      agent
+       .get('/random.php')
+       .expect(200, done)
+    })
+  })
+
+  describe("check the responses from api /filter", function() {
+    it("Тест 1.3", function(done) {
+      agent
+       .get('/filter.php')
+       .expect(200, done)
+    })
+  })
